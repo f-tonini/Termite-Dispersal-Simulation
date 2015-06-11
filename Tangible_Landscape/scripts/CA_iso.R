20925a8fdba1663f08b10706b14a8bf6c823c79c
f-tonini <ftonini84@gmail.com>
2015-06-11 14:52:27 -0400

    removed unnecessary func and fixed alate scenario

--- Tangible_Landscape/scripts/CA_iso.R
+++ Tangible_Landscape/scripts/CA_iso.R
@@ -44,6 +44,7 @@ option_list = list(
   make_option(c("-img","--image"), action="store", default=NA, type='character', help="background satellite raster image for plotting"),
   make_option(c("-s","--start"), action="store", default=NA, type='integer', help="start year"),
   make_option(c("-e","--end"), action="store", default=NA, type='integer', help="end year"),
+  make_option(c("-scn","--scenario"), action="store", default='baseline', type='character', help="alates production scenario (pessimistic = higher and faster alates production over colony life span)"),
   make_option(c("-t","--tab"), action="store", default=NA, type='character', help="look-up table for mating probability"),
   make_option(c("-a","--age"), action="store", default=4, type='integer', help="age of fist alates production"),
   make_option(c("-md","--maxd"), action="store", default=1, type='integer', help="max colony density per grid cell"),
@@ -70,15 +71,14 @@ src_pnt <- readVECT(opt$sources)
 ##background satellite image for plotting
 bkr_img <- raster(paste('./layers/', opt$image, sep='')) 
 
-
 ##Start-End date: 
 start <- opt$start
 end <- opt$end
 if (start > end) stop('start date must precede end date!!')
 
 #build time series for simulation steps:
-dd_start <- as.POSIXlt(as.Date(paste(start,'-01-01',sep='')))
-dd_end <- as.POSIXlt(as.Date(paste(end,'-01-01',sep='')))
+dd_start <- as.POSIXlt(as.Date(paste(start,'-06-01',sep='')))
+dd_end <- as.POSIXlt(as.Date(paste(end,'-06-01',sep='')))
 tstep <- as.character(seq(dd_start, dd_end, 'years'))
 
 #create formatting expression for padding zeros depending on total number of steps
@@ -115,9 +115,9 @@ colonies_lst <- sapply(colonies_rast[], FUN=list)
 #cell ID for pixels with colonies
 idx_col <- cellFromXY(colonies_rast, src_pnt@coords[,1:2])
 #list of all cell IDs for simulation raster
-idx <- sapply(1:ncell(colonies_rast), FUN=list)
+idx_lst <- sapply(1:ncell(colonies_rast), FUN=list)
 #list of colony ages for each cell
-age <- lapply(idx, FUN = function(x) if (x %in% idx_col) src_pnt@data$Age[x == idx_col] else NULL)
+age_lst <- lapply(idx_lst, FUN = function(x) if (x %in% idx_col) src_pnt@data$Age[x == idx_col] else NULL)
 
 #open window screen
 windows(width = 10, height = 10, xpos=350, ypos=50, buffered = FALSE)
@@ -130,11 +130,8 @@ plot(bkr_img)
 xpos <- (bbox(I_rast)[1,2] + bbox(I_rast)[1,1]) / 2
 ypos <- bbox(I_rast)[2,2] - 150
 
-simArea <- init$rr 	
-Age.lst <- init$age 
-Colonies.lst <- init$cl
-Alates <- simArea
-Alates[] <- 0
+alates_rast <- colonies_rast
+alates_rast[] <- 0
 	
 ##LOOP for each year
 for (tt in tstep){
@@ -148,7 +145,7 @@ for (tt in tstep){
     
 	  ##CALCULATE OUTPUT TO PLOT:
 	  # 1) values as % colonized (infected) hectare
-	  colonies_rast[] <- ifelse(colonies_rast[] == 0, NA, round(colonies_rast[]/7, 1))
+	  colonies_rast[] <- ifelse(colonies_rast[] == 0, NA, round(colonies_rast[]/maxdensity, 1))
     
     # 2) number of colonies per hectare
     colonies_rast[] <- ifelse(colonies_rast[] == 0, NA, colonies_rast[])
@@ -168,7 +165,7 @@ for (tt in tstep){
 	  #WRITE TO FILE:
 	  colonies_rast_sp <- as(colonies_rast, 'SpatialGridDataFrame')
 	  writeRAST(colonies_rast_sp, vname=paste(opt$output, '_', sprintf(formatting_str, 0), sep=''), overwrite=TRUE) #write to GRASS raster file
-	  execGRASS('r.timestamp', map=paste(opt$output, '_', sprintf(formatting_str, cnt), sep=''), date=paste(split_date[3], months_names[as.numeric(split_date[2])], split_date[1]))
+	  execGRASS('r.timestamp', map=paste(opt$output, '_', sprintf(formatting_str, cnt), sep=''), date=paste(split_date[3], 'jun', split_date[1]))
 	  
 	  #writeRaster(colonies_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, 0), sep=''), format='HFA', datatype='FLT4S', overwrite=TRUE) # % infected as output
 	  #writeRaster(colonies_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, 0), sep=''), format='HFA', datatype='INT1U', overwrite=TRUE) # nbr. colonies as output
@@ -177,76 +174,72 @@ for (tt in tstep){
 	}else{	
 			
 		##Increase age by 1 in those cells with at least one colony inside
-		Age.lst <- lapply(Age.lst, FUN=function(x){ if (!is.null(x)) x <- x + 1 })
+	  age_lst <- lapply(age_lst, FUN=function(x){ if (!is.null(x)) x <- x + 1 })
 			
 		##Remove colonies over MaxAge...
-		Age.lst <- lapply(Age.lst, FUN=max.age_fun)
+		age_lst <- lapply(age_lst, FUN=max.age_fun)
 			
 		##Whenever a single element within age list has no more colonies inside (died b/c of age),
 		##update the corresponding list element in colony list and set it == 0 
-		Colonies.lst <- mapply(FUN=function(x,y){ if(is.null(x)) y <- 0 else y; return(list(y)) } , Age.lst, Colonies.lst) 
+		colonies_lst <- mapply(FUN=function(x,y){ if(is.null(x)) y <- 0 else y; return(list(y)) } , age_lst, colonies_lst) 
 			
-		if ( !any(unlist(Colonies.lst) > 0) ) {
-			
-			print(paste('No colonies survived at year', year))
-			simArea[] <- unlist(Colonies.lst)
-			writeRaster(simArea > 0, filename=paste('./',fOutput,'/Run',run,'_',year,'.img',sep=''), format='HFA', datatype='LOG1S', overwrite=TRUE)
-			##Estimate the approx. area covered by current colonies in Km^2
-			Tot_area <- cellStats(simArea > 0, stat='sum') * grain  #in units of the grain (meters)
-			Tot_area_km2 <- Tot_area/1000000  
-			area.dataset[run,year-start_time+1] <- Tot_area_km2 
-			colnames(area.dataset)[year-start_time+1] <- year
-			maxyear <- year
-			end_time <- maxyear
-			##EXIT the inner loop and go to the next MC simulation run
-			break
-		}
-			
+		if ( !any(unlist(colonies_lst) > 0) ) stop('Simulation ended. There are no more termite colonies on the landscape!')
+					
 		##Generate total number of offspring per cell by using the age of each existing colony
-		Alates.lst <- lapply(Age.lst, FUN=function(x){if (!is.null(x)) alates.gen(x, scenario='optimistic')})
-		Alates.count <- lapply(Alates.lst, FUN=function(x){if(is.null(x))x <- 0 else sum(x)})
-			
-		Alates[] <- unlist(Alates.count)
-		Alates <- round(focal(Alates, w=as.matrix(probKernel), fun=sum))
+		alates_lst <- lapply(age_lst, FUN=function(x){if (!is.null(x)) alates.gen(x, scenario=opt$scenario)})
+		alates_cnt <- lapply(alates_lst, FUN=function(x){if(is.null(x))x <- 0 else sum(x)})
+
+		alates_rast[] <- unlist(alates_cnt)
+		alates_rast <- round(focal(alates_rast, w=probKernel, fun=sum))
 		
 		##remove alates from unsuitable habitat
-		Alates[habitat_block[] == 1] <- 0
-		
-		NewCol <- Alates
-		##use look-up table now to decide how many new colonies based on alates number in each cell
-		NewCol[] <- sapply(Alates[], FUN=function(x) newCol.gen(x, tab=nCol_table))
+		alates_rast[habitat_block[] == 1] <- 0
 
-		NewCol.lst <- sapply(rasterToPoints(NewCol)[,3], FUN=list)	
+		new.colonies_rast <- alates_rast
+		##use look-up table now to decide how many new colonies based on alates number in each cell
+    new.colonies_rast[] <- sapply(alates_rast[], FUN=function(x) newCol.gen(x, tab=nCol_table))
+    new.colonies_lst <- sapply(new.colonies_rast[], FUN=list)	
 			
-		out <- newCol.addAge(l1 = Age.lst, l2 = Colonies.lst, l3 = NewCol.lst)		
-		Age.lst <- out$age
-		Colonies.lst <- out$cl
+		out <- newCol.addAge(l1 = age_lst, l2 = colonies_lst, l3 = new.colonies_lst)		
+    age_lst <- out$age
+    colonies_lst <- out$cl
 			
-		rm(NewCol.lst)
+		rm(new.colonies_lst)
 			
-		simArea[] <- unlist(Colonies.lst)
-		writeRaster(simArea > 0, filename=paste('./',fOutput,'/Run',run,'_',year,'.img',sep=''), format='HFA', datatype='LOG1S', overwrite=TRUE)
-			
-		#plot(simArea > 0, main = paste("Run ", run, "\n", year, sep=''))
-			
-		##Estimate the approx. area covered by current colonies in Km^2
-		##If writeRas=TRUE a raster file (.img or as defined in source file) is produced
-		Tot_area <- cellStats(simArea > 0, stat='sum') * grain  #in units of the grain (meters)
-		Tot_area_km2 <- Tot_area/1000000  
-		area.dataset[run,year-start_time+1] <- Tot_area_km2 #the starting year the area is always the same in all MonteCarlo model runs
-		colnames(area.dataset)[year-start_time+1] <- year	
-			
-		##Remove objects not needed from the memory
-		rm(Tot_area)
+    colonies_rast[] <- unlist(colonies_lst)
+    
+    ##CALCULATE OUTPUT TO PLOT:
+    # 1) values as % colonized (infected) hectare
+    colonies_rast[] <- ifelse(colonies_rast[] == 0, NA, round(colonies_rast[]/maxdensity, 1))
+    
+    # 2) number of colonies per hectare
+    colonies_rast[] <- ifelse(colonies_rast[] == 0, NA, colonies_rast[])
+    
+    # 3) values as 0 (non infected) and 1 (infected) cell
+    #colonies_rast[] <- ifelse(colonies_rast[] > 0, 1, 0) 
+    #colonies_rast[] <- ifelse(colonies_rast[] > 0, 1, NA) 
+    
+    #PLOT: overlay current plot on background image
+    #bks <- seq(0, 100, by=10)
+    #bks <- seq(0, mx, length = 10)
+    bks <- c(0, 0.25, 0.5, 0.75, 1)
+    #colors <- c("yellow","gold","orange","red")
+    image(colonies_rast, breaks=bks, col=rev(heat.colors(length(bks)-1, alpha=1)), axes=F, ann=F, useRaster=T,add=T, xaxs = "i", yaxs = "i")
+    boxed.labels(xpos, ypos, tt, bg="white", border=NA, font=2)
+    
+    #WRITE TO FILE:
+    colonies_rast_sp <- as(colonies_rast, 'SpatialGridDataFrame')
+    writeRAST(colonies_rast_sp, vname=paste(opt$output, '_', sprintf(formatting_str, 0), sep=''), overwrite=TRUE) #write to GRASS raster file
+    execGRASS('r.timestamp', map=paste(opt$output, '_', sprintf(formatting_str, cnt), sep=''), date=paste(split_date[3], 'jun', split_date[1]))
+    
+    #writeRaster(colonies_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, 0), sep=''), format='HFA', datatype='FLT4S', overwrite=TRUE) # % infected as output
+    #writeRaster(colonies_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, 0), sep=''), format='HFA', datatype='INT1U', overwrite=TRUE) # nbr. colonies as output
+    #writeRaster(colonies_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, 0), sep=''), format='HFA', datatype='LOG1S', overwrite=TRUE)  # 0=non infected 1=infected output
+
 	}
 }
 
-##Print on screen a summary of statistics across all MC runs 
-summary.stats()
-
-##Ask whether the user wants to compute and save an 'occupancy envelope' or not 
-envelope.raster()
-
+message("Spread model finished")
 
 
 
